2fa0a75b99b596622e6e9c076a22e2a6208eeb26
Kim Taeyeong <tyeong1102@gmail.com>
2024-06-27 09:38:47 +0900

    initial commit

---
+++ twofaang/src/main/java/com/twopercent/twofaang/TwofaangApplication.java
@@ -0,0 +1,13 @@
+package com.twopercent.twofaang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwofaangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwofaangApplication.class, args);
+	}
+
+}
---
+++ twofaang/src/test/java/com/twopercent/twofaang/TwofaangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twopercent.twofaang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwofaangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
