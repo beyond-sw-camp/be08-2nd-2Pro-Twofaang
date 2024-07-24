447d895f5895b771c382c5cc1e8af6f0dd65fd2f
mmvne <kingtjrals@naver.com>
2024-07-24 21:23:17 +0900

    회원가입 추가

--- twofaang/src/main/java/com/beyond/twopercent/twofaang/auth/controller/JoinController.java
+++ twofaang/src/main/java/com/beyond/twopercent/twofaang/auth/controller/JoinController.java
@@ -3,14 +3,23 @@ package com.beyond.twopercent.twofaang.auth.controller;
 import com.beyond.twopercent.twofaang.auth.dto.form.JoinDTO;
 import com.beyond.twopercent.twofaang.auth.service.JoinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class JoinController {
     private final JoinService joinService;
+
+    @GetMapping("/user/register")
+    public String joinPage(){
+
+        return "user/register";
+    }
+
+
     @PostMapping("/join")
     public String joinProc(@ModelAttribute JoinDTO joinDto) {
         joinService.join(joinDto);
